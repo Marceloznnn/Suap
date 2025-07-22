523c96245a3c57867a929db81dbd0dfc0f949a16
João Marelo <Nexusstyles9@gmail.com>
2025-07-22 11:00:40 -0300

    commit inicial, atualizando o frontend e backend para do Projeto

--- backend/run.py
+++ backend/run.py
@@ -4,8 +4,9 @@ from app.routes import init_app
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'app', 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'app', 'static')  # <- Adicionado aqui
 
-app = Flask(__name__, template_folder=TEMPLATE_DIR)
+app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.secret_key = 'estagio-2025'
 
 init_app(app)
